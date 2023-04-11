63b018742fe746c12361cae43a743351a996251b
LautaroRossi99 <128433266+LautaroRossi99@users.noreply.github.com>
2023-04-11 01:02:54 -0300

    cambios-finales-js-json

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const contenedorPresentacionHTML = document.getElementById("contenedorPresentacion")
+fetch("../database/vendedores.json")
+    .then(response => response.json())
+    .then(vendedores =>{
+        for(const vendedor of vendedores){
+            contenedorPresentacionHTML.innerHTML += `
+            <div class = "card">
+                <h2>VENDEDOR:</h2>
+                <h2>${vendedor.name} </h2>
+                <p>Edad: ${vendedor.years} </p>
+                <p>Posición: ${vendedor.work} </p>
+                <p>Cargo: ${vendedor.section} </p>
+                <p>Pasion: Jugar Futbol</p>
+                <img src=${vendedor.url}>
+            </div>
+            `
+        }
+    }) 
